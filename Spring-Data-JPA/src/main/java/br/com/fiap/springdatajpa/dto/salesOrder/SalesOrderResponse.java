a113c2bc48d626c8775e29f09ddfac33c78b494d
Your Name <you@example.com>
2019-07-02 20:02:06 -0300

    Merge pull request #4 from bruno-zambotti/master
    Adição de controllers, dto's, advice, entity manager

---
+++ Spring-Data-JPA/src/main/java/br/com/fiap/springdatajpa/dto/salesOrder/SalesOrderResponse.java
@@ -0,0 +1,42 @@
+package br.com.fiap.springdatajpa.dto.salesOrder;
+
+import java.util.List;
+
+public class SalesOrderResponse {
+    Integer id;
+    Integer customerId;
+    Integer status;
+    List<ProductDTO> itens;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public List<ProductDTO> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ProductDTO> itens) {
+        this.itens = itens;
+    }
+}
